914abb63f023d2d77a5b4c929fb38ea52e81c003
yuxilin51 <yuxilin51@gmail.com>
2019-04-24 16:14:39 -0500

    update from home

---
+++ test.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import networkx as nx 
+import matplotlib.pyplot as plt
+
+def main():
+	# print("hello,world")
+	# G = nx.DiGraph()
+	# G.add_node('s')	
+	# G.add_node('a')
+	# G.add_node('b')
+	# G.add_node('c')
+	# G.add_node('d')
+	# G.add_node('e')
+	# G.add_node('f')
+	# G.add_node('t')
+	# G.add_weighted_edges_from([('s','a',3),('s','b',5),('s','c',10),('d','t',7),('e','t',2),('f','t',4)])
+	# G.add_edges_from([('a','d'),('b','d'),('b','f')])
+	# nx.draw_shell(G, with_labels = True)
+	# plt.show()
+	# f_value, f_dict = nx.maximum_flow(G, 's','t',capacity = "weight")
+	# print("flow value ", f_value)
+	# print("flow structure", f_dict)
+
+
+if __name__ == '__main__':
+	main()
